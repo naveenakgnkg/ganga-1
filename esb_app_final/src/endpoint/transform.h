304fd4c6b79acd93e0871ad343471391802c9775
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-23 09:32:26 +0000

    Update transform.h

--- esb_app_final/src/endpoint/transform.h
+++ esb_app_final/src/endpoint/transform.h
@@ -1,5 +1,3 @@
-
-char* get_payload(char *);
-void* get_call_destination(void *urlp,void *apip);
+void apply_transform(char * transform_type, char * transport_key, char * transport_value, char * data_location,char* fields2[]) ;
+int  http_request(char* url);
 int sendemail(char *To, char *path_p);
-
