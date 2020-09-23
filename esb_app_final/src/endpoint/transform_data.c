4a7a550374a50305aaf724ee41cc339249aeec11
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-23 09:31:26 +0000

    Update transform_data.c

--- esb_app_final/src/endpoint/transform_data.c
+++ esb_app_final/src/endpoint/transform_data.c
@@ -1,107 +1,38 @@
-
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
-#include <curl/curl.h>
 
-#define STRING_SIZE 100
-struct string
-{
-  char *ptr;
-  size_t len;
-};
-/*
-void init_string(struct string *s)
-{ 
-  printf("string initialisation\n");
-  s->len = 0;
-  s->ptr = malloc(s->len + 1);
-  if (s->ptr == NULL)
-  {
-    fprintf(stderr, "malloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  s->ptr[0] = '\0';
-}
-*/
-size_t write_callback(void *ptr, size_t size, size_t nmemb, struct string *s)
-{ 
-  printf("Callback is called\n");
-  size_t new_len = s->len + size * nmemb;
-  s->ptr = realloc(s->ptr, new_len + 1);
-  if (s->ptr == NULL)
-  {
-    fprintf(stderr, "realloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->ptr + s->len, ptr, size * nmemb);
-  s->ptr[new_len] = '\0';
-  s->len = new_len;
-  return size * nmemb;
-}
+#include <memory.h>
 
-void* get_call_destination(void *urlp,void *apip)
-{
-  /* Converting URL to string */
-  char *url = (char*) urlp;
-  char *api = (char*) apip;
-  CURL *curl;
-  CURLcode result;
+#include "../BMD/xml_header.h"
 
-
-  curl = curl_easy_init();
-  struct string s;
-  init_string(&s);
-  /* Checks if curl is initialised properly and 
-   * performs required operations */
-
-  if (curl)
-  {
-    printf("Checking Destination Connection %s\n",api);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-    /* Verbose is to check what happens within curl */
-    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-    result = curl_easy_perform(curl);
-
-    if (result != CURLE_OK)
-    {
-      fprintf(stderr, "Connection failed.");
-      exit(0);
+void apply_transform(char * transform_type, char * transport_key, char * transport_value, char * data_location,char* fields2[]) {
+  
+  char * payload=extract_payload(data_location);
+  char payload2[1000];
+  strcpy(payload2,payload);
+  if ((!strcmp(transform_type, ":IFSC_CODE")) && (!strcmp(transport_key, "HTTP"))) {
+    printf("append payload to transport_value i.e. URL\n");
+    
+    for(int i=0;i<strlen(payload2);i++){	
+        char ch = payload2[i];
+	strncat(transport_value, &ch, 1);
     }
 
-    /* always cleanup */
-    curl_easy_cleanup(curl);
+    fields2[0]=transport_value;
+
+  } else if ((!strcmp(transform_type, "JSON")) && (!strcmp(transport_key, "SMTP"))) {
+    printf("converted payload to json file\n");
+    
+    
+
+  } else {
+    printf("No transformation needed\n");
+
+
   }
-  curl_global_cleanup();
-  printf("%s\n",s.ptr);
-  return (void*)s.ptr;
+
 }
 
-char *get_payload(char *url)
-{
-      
-    /*Creates file name of json as per bmd name*/
-    char doc[100]="/home/riya/ganga/esb_app/src/esb/data-Payload.json";
-
-    /* Get data from destination service 
-        that should be sent */
-    char *data = get_call_destination(url,"ifsc");
-
-    FILE *fp;
-    fp = fopen(doc, "w");
-    if (fp == NULL)
-    {
-        printf("file opening failed");
-        exit(0);
-    }
-    /* Writes into json file */
-    fprintf(fp, "\n%s\n", data);
-    /* Closes file */
-    fclose(fp);
-    /* returns json filename */
-    return strdup(doc);
-}
